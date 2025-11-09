97e864bc5586c2e4a3fb0429efd1f717edde746f
Caleb Nierman <calebjnierman@proton.me>
2025-11-08 23:04:35 -0600

    working on calc

--- src/Main.java
+++ src/Main.java
@@ -19,13 +19,10 @@ public class Main {
      */
     public static void main(String[] args) throws FileNotFoundException{
         //String code = "🍌 🌙🍌🌙🌙🍌🌙🌙🌙 🍌🍌🍌🍌🍌🍌 🍌 🌙🍌🍌🌙🌙🍌🌙🍌 🍌🍌🍌🍌🍌🍌";
-        String code = BananaPreprocessor.process(new File("./hello2.txt"));
+        String code = BananaPreprocessor.process(new File("./add.txt"));
         //System.out.println(code);
         BananaParser parser = new BananaParser();
         List<String> commands = parser.parse(code);
-        // List<String> list = new ArrayList<String>();
-        // list.add("PUSH_ONE 72");
-        // list.add("PRINT");
         BananaInterpreter interpreter = new BananaInterpreter();
         interpreter.run(commands);
     }
